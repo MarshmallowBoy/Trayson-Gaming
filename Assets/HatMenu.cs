ca416e79b5551bd116ce792ac7ce074acdc02eea
MarshmallowBoy <traysoncazier719@gmail.com>
2024-11-07 13:22:38 -0800

    Update HatMenu.cs

--- Assets/HatMenu.cs
+++ Assets/HatMenu.cs
@@ -1,10 +1,5 @@
 using UnityEngine;
 using Unity.Netcode;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.Networking;
-using Unity.Networking;
 public class HatMenu : NetworkBehaviour
 {
     public GameObject[] Hat;
