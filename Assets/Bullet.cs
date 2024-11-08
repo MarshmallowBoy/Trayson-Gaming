deb8b5a74f4248f54ba447c2853306c6f1bfa5c4
MarshmallowBoy <traysoncazier719@gmail.com>
2024-11-08 13:45:58 -0800

    Update Bullet.cs

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
