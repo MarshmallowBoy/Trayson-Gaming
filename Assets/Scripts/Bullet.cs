bc463a71011c18b06db43371fe92d690f33611c1
MarshmallowBoy <traysoncazier719@gmail.com>
2024-11-12 12:48:24 -0800

    Update Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,16 +5,17 @@ public class Bullet : MonoBehaviour
     public int Damage = 10;
     public bool Collision = false;
     public ulong ID;
-
+    public bool Damaged = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !Damaged)
         {
             if (other.GetComponent<FishThrower>().ID == ID)
             {
                 return;
             }
             other.gameObject.GetComponent<Heath>().health -= Damage;
+            Damaged = true;
             Destroy(gameObject);
         }
         if(!other.CompareTag("Player") && !other.CompareTag("Bullet"))
