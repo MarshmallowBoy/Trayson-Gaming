df5973077139d4cbb5bc98de0c261404131271e1
MarshmallowBoy <traysoncazier719@gmail.com>
2024-11-18 13:51:17 -0800

    Changed Force Equation

--- Assets/ExplosiveFish.cs
+++ Assets/ExplosiveFish.cs
@@ -15,12 +15,12 @@ public class ExplosiveFish : MonoBehaviour
         {
             if (Coll.CompareTag("Player"))
             {
-                Coll.gameObject.GetComponent<SC_FPSController>().ExternalVector = (((Coll.transform.position - transform.position).normalized + DirectionBias) * force) / (Vector3.Distance(Coll.transform.position, transform.position)+1);
+                Coll.gameObject.GetComponent<SC_FPSController>().ExternalVector = (((Coll.transform.position - transform.position).normalized + DirectionBias) * force) / (((Vector3.Distance(Coll.transform.position, transform.position)+1))/ExplosionRadius);
                 if(Coll.transform.GetComponent<FishThrower>().ID == GetComponent<Bullet>().ID)
                 {
                     return;
                 }
-                Coll.transform.GetComponent<Heath>().health -= Mathf.RoundToInt(ExplosionDamage / (Vector3.Distance(Coll.transform.position, transform.position)));
+                Coll.transform.GetComponent<Heath>().health -= Mathf.RoundToInt(ExplosionDamage / ((Vector3.Distance(Coll.transform.position, transform.position))/ExplosionRadius));
             }
         }
         
