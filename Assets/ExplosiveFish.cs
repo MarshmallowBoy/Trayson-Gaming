a0292f31422b2c606278f285894bfbc6905ab214
MarshmallowBoy <traysoncazier719@gmail.com>
2024-11-18 13:08:43 -0800

    Made it so that your own explosions can't hurt you.

--- Assets/ExplosiveFish.cs
+++ Assets/ExplosiveFish.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ExplosiveFish : MonoBehaviour
@@ -6,6 +7,7 @@ public class ExplosiveFish : MonoBehaviour
     public float ExplosionRadius;
     public float force;
     public Vector3 DirectionBias;
+    public float ExplosionDamage;
     private void OnDestroy()
     {
         Instantiate(Explosion, transform.position, Quaternion.identity);
@@ -14,6 +16,11 @@ public class ExplosiveFish : MonoBehaviour
             if (Coll.CompareTag("Player"))
             {
                 Coll.gameObject.GetComponent<SC_FPSController>().ExternalVector = (((Coll.transform.position - transform.position).normalized + DirectionBias) * force) / (Vector3.Distance(Coll.transform.position, transform.position)+1);
+                if(Coll.transform.GetComponent<FishThrower>().ID == GetComponent<Bullet>().ID)
+                {
+                    return;
+                }
+                Coll.transform.GetComponent<Heath>().health -= Mathf.RoundToInt(ExplosionDamage / (Vector3.Distance(Coll.transform.position, transform.position)));
             }
         }
         
