42e7d69817d7fcdef9a39f740e1eaa7f985ace72
MarshmallowBoy <traysoncazier719@gmail.com>
2024-11-20 12:09:39 -0800

    DamageScriptOnTrigger

---
+++ Assets/Scripts/Special/DamageOnTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class DamageOnTrigger : MonoBehaviour
+{
+    public int Damage;
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            other.transform.GetComponent<Heath>().health = Damage;
+        }
+    }
+}
