ecf3ad11d58c5b1858fd18c6eb6fb4e7faa01ecb
MarshmallowBoy <traysoncazier719@gmail.com>
2025-01-26 01:44:33 -0800

    Tank still broken, added item stuff for steamworks though for cosmetics

--- Assets/SteamScriptTest.cs
+++ Assets/SteamScriptTest.cs
@@ -5,11 +5,13 @@ public class SteamScriptTest : MonoBehaviour
 {
     public TextMeshProUGUI textMeshProUGUI;
     protected Callback<GameOverlayActivated_t> m_GameOverlayActivated;
+    protected Callback<UserStatsReceived_t> m_UserStatsReceived;
     private void OnEnable()
     {
         if (SteamManager.Initialized)
         {
             m_GameOverlayActivated = Callback<GameOverlayActivated_t>.Create(OnGameOverlayActivated);
+            m_UserStatsReceived = Callback<UserStatsReceived_t>.Create(GiveStartAchievement);
         }
     }
 
@@ -24,6 +26,37 @@ public class SteamScriptTest : MonoBehaviour
             Debug.Log("Steam Overlay has been closed");
         }
     }
+
+    public void GiveStartAchievement(UserStatsReceived_t pCallback)
+    {
+        if (SteamUserStats.SetAchievement("starting_achievement"))
+        {
+            SteamUserStats.StoreStats();
+        }
+    }
+
+    public void GrantTestItems()
+    {
+        SteamItemDef_t[] newItems = new SteamItemDef_t[1];
+        uint[] quantities = new uint[1];
+
+        newItems[0] = (SteamItemDef_t)1;
+        quantities[0] = 1;
+
+        SteamInventoryResult_t resultHandle;
+
+        if (SteamInventory.GenerateItems(out resultHandle, newItems, quantities, (uint)newItems.Length))
+        {
+            Debug.Log("Items generated successfully.");
+            // Do something with resultHandle if needed, like fetching the item details.
+            SteamInventory.DestroyResult(resultHandle); // Clean up after you're done.
+        }
+        else
+        {
+            Debug.LogError("Failed to generate items.");
+        }
+    }
+
 
     void Start()
     {
