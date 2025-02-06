b59fd18ea4fc89eaf0a77cb1b72bdac389bdcbf2
MarshmallowBoy <traysoncazier719@gmail.com>
2025-02-06 13:39:40 -0800

    Added Aiming System To Tank

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 public class Tank : MonoBehaviour
 {
     public Rigidbody body;
+    public Transform TurretBody;
+    public Transform TurretGun;
+    Vector3 Target;
     public float acceleration;
     public float speed;
     public float angularAcceleration;
     public float angularSpeed;
+    public float turretSpeed;
     public BlendShapesController BSC;
     public float TreadAnimSpeed = 1;
+    public ParticleSystem MuzzleFlash;
+    public float Delay;
+    float nextTimeToFire;
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     void Update()
@@ -25,7 +32,28 @@ public class Tank : MonoBehaviour
         {
             body.angularVelocity += new Vector3(0, Input.GetAxis("Horizontal") * angularAcceleration, 0);
         }
-        
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit _mouseRay;
+        if (Physics.Raycast(ray, out _mouseRay, Mathf.Infinity))
+        {
+            if (_mouseRay.point != null)
+            {
+                Target = _mouseRay.point;
+            }
+        }
+
+        if (Input.GetButton("Fire1") && nextTimeToFire < Time.time)
+        {
+            nextTimeToFire = Time.time + Delay;
+            RaycastHit hit;
+            if (Physics.Raycast(TurretGun.position, -TurretGun.forward, out hit))
+            {
+                MuzzleFlash.Play();
+            }
+        }
+
+        Debug.DrawRay(TurretGun.position, -TurretGun.forward * 1000, Color.red);
     }
 
     private void FixedUpdate()
@@ -39,6 +67,10 @@ public class Tank : MonoBehaviour
             BSC.rightProgress += Input.GetAxis("Vertical") * TreadAnimSpeed;
             BSC.leftProgress += Input.GetAxis("Vertical") * TreadAnimSpeed;
         }
+
+
         
+        TurretBody.Rotate(0, TurretBody.InverseTransformPoint(Target).x * turretSpeed, 0);
+        TurretGun.Rotate(TurretGun.InverseTransformPoint(Target).y * turretSpeed, 0, 0);
     }
 }
