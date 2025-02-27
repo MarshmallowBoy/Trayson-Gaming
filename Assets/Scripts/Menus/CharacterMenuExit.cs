be3fdae567a9f1148b1bf83a7d7658ecdfcc7b7e
DraconicLegacy <richie.weldon@icloud.com>
2025-02-27 13:01:32 -0800

    Updated CharacterMenu Functions

---
+++ Assets/Scripts/Menus/CharacterMenuExit.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class CharacterMenuExit : MonoBehaviour
+{
+    Animation anim;
+}
--- Assets/UIAnimController.cs
+++ Assets/UIAnimController.cs
@@ -3,8 +3,30 @@ using UnityEngine;
 public class UIAnimController : MonoBehaviour
 {
     public GameObject CharacterMenu;
+    public AudioSource cMenuAS;
+    public Animator animr;
+
     public void EnableCharacterMenu()
     {
         CharacterMenu.SetActive(true);
+        if(cMenuAS ==  null) { GameObject.Find("CharacterMenu").GetComponent<AudioSource>(); cMenuAS.Play(); }
+        else { cMenuAS.Play(); }
+    }
+
+    public void DisableCharacterMenu()
+    {
+        if (cMenuAS == null) { GameObject.Find("CharacterMenu").GetComponent<AudioSource>(); cMenuAS.Stop(); }
+        else { cMenuAS.Stop(); }
+        CharacterMenu.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && CharacterMenu.activeInHierarchy == true)
+        {
+            if(animr ==  null) { animr = GameObject.Find("AnimationAndFades").GetComponent<Animator>(); }
+            animr.SetTrigger("Exit");
+        }
+        else { return; }
     }
 }
